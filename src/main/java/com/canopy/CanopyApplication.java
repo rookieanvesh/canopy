8696c9747f839500cc646d8f66f118cb24b13bd3
rookieanvesh <srivastavaanvesh13@gmail.com>
2025-01-27 21:27:48 +0530

    initial setup

---
+++ src/main/java/com/canopy/CanopyApplication.java
@@ -0,0 +1,13 @@
+package com.canopy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanopyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CanopyApplication.class, args);
+	}
+
+}
